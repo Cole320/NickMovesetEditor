07b5d7c751566613dbabe8ea16fb896ba7ddd5ad
Cole320 <colebrazinski15@gmail.com>
2021-11-12 15:46:36 -0500

    remove bulid files and binaries

--- NmeCompanionPlugin/obj/Debug/net48/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NmeCompanionPlugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "NmeCompanionPlugin";
-        public const string PLUGIN_NAME = "NmeCompanionPlugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
